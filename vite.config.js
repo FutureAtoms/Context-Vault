70b23f209e09e6cede6d4be68c9038347407ad4e
Abhilash Chadhar <abhilashchadhar@gen10.58.33.83.ddns.hightechcampus.nl>
2025-07-04 11:53:49 +0200

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/context-vault/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Context-Vault/' : '/',
   server: {
     port: 3000,
     open: true
